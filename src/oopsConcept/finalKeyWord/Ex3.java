4587c2fba2a2c54a701871220d651b8aa4e88675
Govind <govind.krcrimfo555@gmail.com>
2025-10-10 03:20:27 +0530

    Final variable property

---
+++ src/oopsConcept/finalKeyWord/Ex3.java
@@ -0,0 +1,11 @@
+package oopsConcept.finalKeyWord;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        final int i;
+        i = 10;
+
+        System.out.println(i);
+    }
+
+}
