a1717d7439881da1c093e2cc3b613011afb99fd5
Govind <govind.krcrimfo555@gmail.com>
2025-10-10 02:49:12 +0530

    final key word example moved to package finaKeyWord

---
+++ src/oopsConcept/finalKeyWord/Ex1.java
@@ -0,0 +1,40 @@
+package oopsConcept;
+
+interface car {
+    public void cardetails();
+}
+
+abstract class cycle {
+    abstract void bycledetails();
+
+}
+
+final class Test extends cycle implements car {
+
+    public void greet() {
+        System.out.println("hello from greet method");
+    }
+
+    @Override
+    public void cardetails() {
+        System.out.println("Car details");
+    }
+
+    @Override
+    void bycledetails() {
+        System.out.println("Bycycle details ");
+    }
+
+}
+
+class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Final keyword ");
+        Test t = new Test();
+        t.greet();
+        t.bycledetails();
+        t.cardetails();
+        System.out.println();
+
+    }
+}
